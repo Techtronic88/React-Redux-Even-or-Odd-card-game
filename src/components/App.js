d2d781333e90010a405b9c91bb4012b52f1fce84
DoanThomas88 <38048770+Techtronic88@users.noreply.github.com>
2019-06-04 19:31:45 +0700

    Add files via upload
    Changes made to this commit
    - refactored mapDispatchtoProps code again to make it much easier to understand as pulling data from Fetch Json API was difficult to understand so I chose this way to write code for now  for it's easier to keep track.

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
-import {startGame,  cancelGame} from "../action/setting";
+import {startGame, cancelGame }  from "../action/setting";
 import Instruction from "./Instruction"
-
+import { fetchDeckResult } from "../action/deck"
 
 
 class App extends Component {
+
+// NOTE: Do not confuse this startGame function with startGame Action creators
+// In this APP component onCLick={ this.startGame } as we are creating a brandnew function in this component
+// inside  StartGame function we then call startGame action creators objects
+startGame = () => {
+  this.props.startGame();
+
+  fetch("https://deckofcardsapi.com/api/deck/new/shuffle")
+  .then(res => res.json())
+  .then(json => this.props.fetchDeckResult(json))
+} 
   
-// THIS IS only use for when we use mapStateToProps parameter of connect()  
-// gameCancel = () => { return this.props.dispatch(cancelGame())}
-// beginGame = () => { return this.props.dispatch(startGame())}
+
 render() {
 console.log('this', this)
     return (
@@ -24,7 +33,7 @@ console.log('this', this)
                   ): (
                     <div>
                         <h3> The game is done  </h3><br/>
-                        <button onClick={this.props.startGame}>Start Game</button>
+                        <button onClick={this.startGame}>Start Game</button>
                     </div>
                   )
                 }
@@ -35,20 +44,30 @@ console.log('this', this)
   }
 }
 
-// JUST A QUICK NOTE: We can shorten this code 
-// const mapStateToProps = state => {
-//    console.log("state", state );
-//       return {gameStarted: state.gameStarted };
-// }
 
-// const mapDispatchtoProps =  {
-//     startGame,
-//     cancelGame 
-// }
+const mapStateToProps = state => {
+   console.log("state", state );
+      return {gameStarted: state.gameStarted };
+}
 
-//export default connect(mapStateToProps, mapDispatchtoProps) (App);
+// takes dispatch METHOD of REDUX store.dispatch 
+// When mapDispatchToProps function REDUX passes in dispatch under the hood
+// We return the action inside of mapDispatchToProps
+// We will loop through each of those actions -> attach dispatch to it 
+// RESULT: we now can directly access those actions via this.props.action 
 
-// Alternative we can use this way as well 
-export default connect ( state =>  ({ gameStarted: state.gameStarted }),
-{startGame, cancelGame}) (App);
 
+// SINCE fetchDeckResult take in a Json parameter, we NEED to account for that in mapDispatchToProps WHY ???
+// because that deckJson is argument passed in = json passed in 
+// Store.dispatch needs to bind this properties so we can access them via props.
+// startGame and cancelGame did not need any argument passed in BECAUSE we made the dummy data it was simple enough
+// fetchDeckResult does not have that luxury
+
+const mapDispatchtoProps = dispatch =>  {
+      return {
+        startGame: () => dispatch(startGame()),
+        cancelGame: () => dispatch(cancelGame()),
+        fetchDeckResult: deckJson => dispatch(fetchDeckResult(deckJson))
+      }
+}
+export default connect(mapStateToProps, mapDispatchtoProps) (App);
