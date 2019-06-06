caabb4f67252351cf59da646707aa976c3b61771
DoanThomas88 <38048770+Techtronic88@users.noreply.github.com>
2019-06-06 17:41:56 +0700

    Add files via upload

--- src/action/deck.js
+++ src/action/deck.js
@@ -1,13 +1,26 @@
-import  { FETCH_DECK_RESULT } from "./types"
+import  { DECK } from "./types"
 
 // Problem we have right now:
 // We have type but the data we will need to pull in from fetch / cannot create our own
 // NOTE: deckJson represents JSON data from a successful FETCH = remaining, deck_id 
 // 
-export const fetchDeckResult = deckJson => {
+export const fetchDeckSuccess = deckJson => {
     const { remaining, deck_id } = deckJson
-    return { type: FETCH_DECK_RESULT, remaining, deck_id }
+    return { type: DECK.FETCH_SUCCESS, remaining, deck_id }
 }
 
+
+export const fetchDeckError = error => {
+  return { type: DECK.FETCH_ERROR, message: error.message}
+}
+// Convering previous fetch to Reuseable Fetch 
+// Notice we won't need to use this.props.fetchDeckResult anymore
+// Intead we can just use this.fetchDeckResult
+export const fetchNewDeck = dispatch => {
+   return fetch("https://deck-of-cards-api-wrapper.appspot.com/deck/new/shuffle")
+  .then(res => res.json())
+  .then(json => dispatch(fetchDeckSuccess(json)))
+  .catch(error => dispatch(fetchDeckError(error)))
+}
 
 
